9cb31ee636a0d30eb8b6b402721335772fdbd88d
madpang <133752527+madpang@users.noreply.github.com>
2025-05-21 23:46:50 +0900

    Parse the header info.
    Header info. is a MUST requirement for a mmd text content.

--- README.txt
+++ README.txt
@@ -3,3 +3,7 @@ Convert custom plain text markup to HTML.
 --- Step 0
 
 Wrap raw text in <pre> tags---Yes, from text to HTML!
+
+--- Step 1
+
+Parse the header of the content file.
--- src/mmd2html.ps1
+++ src/mmd2html.ps1
@@ -2,14 +2,14 @@
 <#
 	@brief: Convert custom plain text markup to HTML.
 
-    @details: Just wrap the input text in <pre> tag
+	@details: Just wrap the input text in <pre> tag
 
 	@author
 	- madpang
 
 	@date:
 	- Created on 2025-05-17
-	- Updated on 2025-05-17
+	- Updated on 2025-05-21
 #>
 
 param(
@@ -17,18 +17,49 @@ param(
 )
 
 # === Read the input text
-$in_lines = Get-Content -Path $path2txt
+$in_lines_ = Get-Content -Path $path2txt
 
 # === Initialize the output HTML formatted lines
-$out_lines = @()
+$out_lines_ = @()
 
 # === Processing
-$out_lines += "<pre>"
-for ($ii = 0; $ii -lt $in_lines.Count; $ii++) {
-    $line = $in_lines[$ii]
-    $out_lines += $line
+if ($in_lines_.Count -eq 0) {
+	Write-Error "Input file is empty"
+	exit 1
 }
-$out_lines += "</pre>"
+if ($in_lines_[0] -notmatch '^\+{3} header$') {
+	Write-Error "Every post must provide header info."
+	exit 2
+}
+# --- Parse the header
+# Track the processed line number
+$ln_track_ = 1
+# Initialize a hashtable to hold header meta info
+$meta_info_ = @{ title = $null }
+for ($ln = 1; $ln -lt $in_lines_.Count; $ln++) {
+	$line = $in_lines_[$ln]
+	if ($line -match '^\+{3}$') {
+		# Header ends
+		$ln_track_ = $ln
+		break
+	}
+	if ($line -match '^@title:\s*(.*)$') {
+		$meta_info_.title = $Matches[1].Trim()
+	}
+}
+if (-not $meta_info_.title) {
+	Write-Error "MISSING META INFO: @title"
+	exit 3
+}
+# --- Parse the content
+$out_lines_ += "<h1>$($meta_info_.title)</h1>"
+$out_lines_ += "<pre>"
+for ($ln = $ln_track_+1; $ln -lt $in_lines_.Count; $ln++) {
+	$line = $in_lines_[$ln]
+	$out_lines_ += $line
+}
+$out_lines_ += "</pre>"
 
 # === Output the HTML formatted lines
-Write-Output $out_lines
+Write-Output $out_lines_
+exit 0
